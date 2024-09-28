87483152b373aa0ffee43ea600b4cd6133f4c8f4
Moshe Rivkin <m0535541179@gmail.com>
2024-09-28 13:11:06 +0300

    init site

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from "astro/config";
+import icon from "astro-icon";
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    icon(),
+  ],
+});
---
+++ src/config.js
@@ -0,0 +1,16 @@
+export const config = {
+  title: "חיים ישראלי | האתר הרשמי",
+  logo: "HI.",
+  header: {
+    h1: "שלום, שמי חיים ישראלי",
+    h2: " יועץ אירגוני ומנהל פרויקטים",
+    description: "אני גם מספר בדיחות אבא בזמני הפנוי",
+  },
+  contact: {
+    email: "chaimisraeli@google.com",
+    phone: "+123456789",
+    youtube: "@IsraelMoDonline",
+    facebook: "MODIsrael",
+    twitter: "MoDIsrael",
+  },
+};
